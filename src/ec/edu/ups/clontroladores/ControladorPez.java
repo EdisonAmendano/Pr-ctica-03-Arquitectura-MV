4a4a22364c4417f741b0fe9393dd0e3a0171c106
Edison <Edison@MSI>
2019-04-23 15:56:46 -0500

    implementación usando la interface List y la clase ArrayList.

--- src/ec/edu/ups/clontroladores/ControladorPez.java
+++ src/ec/edu/ups/clontroladores/ControladorPez.java
@@ -5,10 +5,68 @@
  */
 package ec.edu.ups.clontroladores;
 
+import ec.edu.ups.clases.Pez;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Edison
  */
 public class ControladorPez {
+
+    private List<Pez> lista;
+    private int codigo;
+
+    public ControladorPez() {
+        codigo = 0;
+        lista = new ArrayList<>();
+    }
+
+    public void create(Pez objeto) {
+        codigo++;
+        objeto.setCodigo(codigo);
+        lista.add(objeto);
+    }
+
+    public Pez read(int codigo) {
+        for (Pez pez : lista) {
+            if (pez.getCodigo() == codigo) {
+                return pez;
+            }
+        }
+        return null;
+    }
+
+    public void update(Pez objeto) {
+        for (int i = 0; i < lista.size(); i++) {
+            Pez elemento = lista.get(i);
+            if (elemento.getCodigo() == objeto.getCodigo()) {
+                lista.set(i, objeto);
+            }
+        }
+    }
+
+    public void delet(int codigo) {
+        for (int i = 0; i < lista.size(); i++) {
+            Pez elemento = lista.get(i);
+            if (elemento.getCodigo() == codigo) {
+                lista.remove(i);
+                break;
+            }
+        }
+    }
     
+    public void listar (){
+        for (Pez pez : lista) {
+            System.out.println(pez);
+        }
+    }
+    
+    public void codigo(){
+       for (Pez pez : lista) {
+            System.out.println(pez.getCodigo()+". "+pez.getNombre());
+        } 
+    }
 }
+
